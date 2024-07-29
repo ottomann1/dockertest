daf920306bc32d204b9a3d15c97fa7baaf4c9640
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-29 11:43:51 +0200

    this is called structured logging?

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const logger = require('./logger');
 const app = express();
 const router = express.Router();
 
@@ -12,20 +12,23 @@ router.use(function (req, res, next) {
 });
 
 router.get("/status", function (req, res) {
+  logger.info("status checked")
   res.status(200).send();
 });
 
 app.post('/payments', (req, res) => {
-  console.log(req.body);
+  logger.info("payment received: ", req.body);
   const { carId, amount } = req.body;
   if (typeof amount !== 'number' || !Number.isInteger(amount)) {
+    logger.info('Invalid amount. It must be an integer.')
     return res.status(400).json({ error: 'Invalid amount. It must be an integer.' });
   }
+  logger.info({ message: 'Payment processed successfully', carId, amount })
   res.status(201).json({ message: 'Payment processed successfully', carId, amount });
 });
 
 app.use("/", router);
 
 app.listen(port, function () {
-  console.log("Example app listening on port 8080!");
+  logger.info("Example app listening on port 8080!");
 });
