66b12d80ac3dfde75a50a216a0e001f2816e3006
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-30 11:32:31 +0200

    formatsdfasdafsdsdfsd

--- logger.ts
+++ logger.ts
@@ -5,7 +5,7 @@ const { combine, timestamp, label, printf, colorize, json, prettyPrint } =
 
 const logger = winston.createLogger({
   level: "info", // Set the default log level
-  format: combine(winston.format.json(), winston.format.colorize()),
+  format: winston.format.json(),
   transports: [
     // Log to the console
     new winston.transports.Console({
