545601227232740bf82ba42fb0cb5f44c456732a
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-30 10:33:39 +0200

    format test json, colorized, splat

--- logger.ts
+++ logger.ts
@@ -3,13 +3,17 @@ import winston from "winston";
 const { format } = winston;
 const { combine, timestamp, label, printf, colorize, simple } = format;
 
-const myFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`;
-});
+// const myFormat = printf(({ level, message, label, timestamp }) => {
+//   return `${timestamp} [${label}] ${level}: ${message}`;
+// });
 
 const logger = winston.createLogger({
   level: "info", // Set the default log level
-  format: winston.format.json(),
+  format: combine(
+    format.json(),
+    format.colorize({ all: true }),
+    format.splat(),
+  ),
   transports: [
     // Log to the console
     new winston.transports.Console({
